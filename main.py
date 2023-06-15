cc1e6e9e11aeec69e24e1bba2b2a886d4f5eebe2
Muhammad  Umar <umarrajput930@gmail.com>
2023-06-15 20:09:14 +0500

    getitng started

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print ('Hello from local')
-print ('done')
-print ('my first github test')
+# Download Wordbank from nltk. you can create your own words list
+
+# import nltk
+# nltk.download('wordnet')
+
+from nltk.corpus import wordnet
+
+word_bank = wordnet.words()
+
+words = list(word_bank)
+
+print (words)
